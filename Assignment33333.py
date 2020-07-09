68fcb8f5711c3a6c4cccc4f5e21382ddb0bacfec
Brightjnr <67961936+Brightjnr@users.noreply.github.com>
2020-07-09 15:56:42 +0000

    Add files via upload

---
+++ Assignment33333.py
@@ -0,0 +1,19 @@
+#Bright Okrah Junior
+#Assignment 0
+def grade(x):
+     if 90<=x<=100:
+         return 'A'
+     elif 80<=x<=89:
+         return 'B'
+     elif 70<=x<=79:
+       return 'C'
+     elif 60<=x<=69:
+         return 'D'
+     else:
+         return 'F'
+
+
+
+
+    
+    
